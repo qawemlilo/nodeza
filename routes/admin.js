c524820dfb36c4916acd7e8ab1649d09af852d3e
Qawelesizwe <qawemlilo@gmail.com>
2015-02-03 16:17:00 +0200

    added pull hook

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 "use strict";
 
-/* 
+/*
  * Account Routes
  * passing 'passport.isNotAuthenticated' middle to avoid logged in users from viewing some pages
 **/
 
 var auth = require('../lib/auth');
+var exec = require('child_process').exec;
 
 
 module.exports = function (app, AdminController) {
@@ -14,4 +15,15 @@ module.exports = function (app, AdminController) {
   app.get('/admin/settings', auth.isUserAdmin, AdminController.getGlobalConfig);
   app.get('/admin/settings/server', auth.isUserAdmin, AdminController.getServerConfig);
   app.get('/admin/settings/account', auth.isUserAdmin, AdminController.getAccountConfig);
+
+  app.post('/pull', function (req, res) {
+    exec('cd /var/repos/nodeza && git pull', function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+  });
 };

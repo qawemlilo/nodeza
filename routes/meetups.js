881f55e7dc6c701d7cf6c1deb16cb9e3c9704517
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2016-04-21 16:03:50 +0200

    brought back meetups

---
+++ routes/meetups.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const App = require('widget-cms');
+const MeetupsController = App.getController('Meetups');
+const auth = require('../lib/auth');
+
+App.get('/admin/meetups', auth.isAuthenticated, MeetupsController.getAdmin);
+App.get('/admin/meetups/settings', auth.isUserAdmin, MeetupsController.getSettings);
+App.get('/meetups/new', auth.isAuthenticated, MeetupsController.getNew);
+App.get('/meetups/edit/:id', auth.isAuthenticated, MeetupsController.getEdit);
+App.post('/meetups/new', auth.isAuthenticated, MeetupsController.postNew);
+App.post('/meetups/edit', auth.isAuthenticated, MeetupsController.postEdit);
+
+// public
+App.get('/meetups/:slug', MeetupsController.getMeetup);
+App.get('/meetups', MeetupsController.getMeetups);

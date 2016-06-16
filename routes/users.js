a8c58b8e9e628da576e3d55bf0e7358764a3ea34
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2016-06-16 11:31:26 +0200

    widget-cms update

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,14 @@
 
 const App = require('widget-cms');
 const UsersController = App.getController('Users');
+const auth = require('../lib/auth');
 
 
-App.get('/admin/users', UsersController.getUsers);
-App.get('/users/new', UsersController.getNewUser);
-App.get('/users/edit/:id', UsersController.getEditUser);
-App.post('/users/new', UsersController.postUser);
-App.post('/users/edit', UsersController.postEditUser);
-App.get('/users/delete/:id', UsersController.getDeleteUser);
-App.get('/devs/:slug', UsersController.getProfile);
-App.get('/devs', UsersController.getDevs);
-App.get('/devs', UsersController.getDevs);
+App.get('/admin/users', auth.isUserAdmin, UsersController.getUsers);
+App.get('/users/new', auth.isUserAdmin, UsersController.getNewUser);
+App.get('/users/edit/:id', auth.isUserAdmin, UsersController.getEditUser);
+App.post('/users/new', auth.isUserAdmin, UsersController.postUser);
+App.post('/users/edit', auth.isUserAdmin, UsersController.postEditUser);
+App.get('/users/delete/:id', auth.isAuthenticated, UsersController.getDeleteUser);
+App.get('/devs/:slug', auth.isAuthenticated, UsersController.getProfile);
+App.get('/devs', auth.isAuthenticated, UsersController.getDevs);

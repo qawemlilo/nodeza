a349b9a498f65ccdecc6e8965b678e41ae754706
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2018-03-17 19:08:48 +0200

    added bot for tweeting new package releases

---
+++ bots/tweet-package-releases.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const Twitter = require('twit');
+const axios = require('axios');
+const moment = require('moment');
+const util = require('util');
+const config = require('../.env');
+
+
+const client = new Twitter({
+  consumer_key: config.TWITTER_BOT_KEY,
+  consumer_secret: config.TWITTER_BOT_SECRET,
+  access_token: config.TWITTER_BOT_TOKEN_KEY,
+  access_token_secret: config.TWITTER_BOT_TOKEN_SECRET
+});
+
+
+console.log();
+console.log(' > Checking for new packages releases');
+console.log();
+
+
+async function findAndTweetNewReleases(name, url) {
+  try {
+    let res = await axios.get(url);
+    let releases = res.data;
+
+    if (releases && releases.length) {
+      releases.forEach(function (release) {
+        let hoursAgo = moment().diff(moment(release.published_at), 'hours');
+
+        if (hoursAgo < 24) {
+        let tweet = `New #${name} release: ${release.tag_name} - ${release.html_url}`;
+
+          tweeterClient.post('statuses/update', { status: tweet }, function(err, data, response) {
+            if (error) {
+              console.error(error)
+            }
+          });
+        }
+      });
+    }
+
+    return true;
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+
+// nodejs
+findAndTweetNewReleases('nodejs', 'https://api.github.com/repos/nodejs/node/releases');
+
+// express
+findAndTweetNewReleases('express', 'https://api.github.com/repos/expressjs/express/releases');
